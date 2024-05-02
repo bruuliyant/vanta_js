b04853f40a92b8b2211ec71931d7bf1f2b8edaf2
bruuliyant <146166750+bruuliyant@users.noreply.github.com>
2024-05-02 19:48:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+VANTA.CLOUDS({
+    el: "#element",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    skyColor: 0x9db6c3,
+    cloudColor: 0xbabac5,
+    sunColor: 0xd9d9d9,
+    sunGlareColor: 0x0,
+    sunlightColor: 0x0,
+    speed: 1.40
+  })
